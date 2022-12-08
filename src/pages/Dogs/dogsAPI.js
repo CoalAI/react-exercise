4ca1c6b8cb3b92e8224da53b68a3a509f858bfc0
ammar <ammarhaider001@github.com>
2022-12-08 22:53:53 +0000

    removed unnecessary code

--- src/pages/Dogs/dogsAPI.js
+++ src/pages/Dogs/dogsAPI.js
@@ -1,6 +1,6 @@
-import React, { useState, useMemo, setState, useEffect } from "react";
-import { Breadcrumb, Layout, Menu } from "antd";
-import { Button, Col, Form, Input, Row, Select, Checkbox } from "antd";
+import React, { useState, useEffect } from "react";
+import { Layout} from "antd";
+import { Button, Form, Select } from "antd";
 const { Option } = Select;
 const { Header, Content, Footer } = Layout;
 import { fetchBreeds, fetchSubBreeds, fetchSubbreedImages } from "../../apis";
@@ -19,13 +19,11 @@ export default function Dogs() {
 
   useEffect(() => {
     fetchBreeds().then((_breeds) => {
-      console.log("_breeds", _breeds);
       var breedsArray = Object.keys(_breeds).map(breed => ({
         value: breed,
         label: breed
-      })) 
+      }))
       setBreeds(breedsArray)
-      console.log("breeds", breedsArray);
     }
     );
   }, []);
@@ -33,86 +31,79 @@ export default function Dogs() {
     setSubBreeds([]);
     setSelectedSubBreed("");
     setBreedError(false);
-    // setImages([]);
-    // setSelectedNoImages(0);
-    if(selectedBreed){
+    
+    if (selectedBreed) {
       fetchSubBreeds(selectedBreed).then((_subBreeds) => {
-        console.log("_subBreeds", _subBreeds);
         var subBreedsArray = _subBreeds.map(sub_breed => ({
           value: sub_breed,
           label: sub_breed
-        })) 
-        if(subBreedsArray.length == 0){
+        }))
+        if (subBreedsArray.length == 0) {
           fetchSubbreedImages(selectedBreed).then(arr_images => {
             setImages(arr_images)
           });
-        }else{
+        } else {
           setSubBreeds(subBreedsArray)
-          console.log("Subbreeds", subBreedsArray);
         }
-        
+
       }
       );
     }
   }, [selectedBreed]);
-useEffect(()=>{
-  // setImages([]);
-  // setSelectedNoImages(0);
-  setSubBreedError(false);
-  console.log("selectedBreed", selectedBreed);
-  console.log("selectedSubBreed", selectedSubBreed);
-  if(selectedSubBreed.length){
-    fetchSubbreedImages(selectedBreed, selectedSubBreed).then(arr_images => {
-      setImages(arr_images)
-    });
-  }
-},[selectedSubBreed])
 
-useEffect(()=>{
-  setImages([]);
-  setSelectedNoImages(0);
-},[breeds, SubBreeds])
+  useEffect(() => {
+    setSubBreedError(false);
+    if (selectedSubBreed.length) {
+      fetchSubbreedImages(selectedBreed, selectedSubBreed).then(arr_images => {
+        setImages(arr_images)
+      });
+    }
+  }, [selectedSubBreed])
 
-useEffect(()=>{
-  setImagesError(false);
-}, [noImages])
-const imageOptions = images.map((image, index)=>{
-  return {
-    value: index + 1,
-    lable: index + 1
-  }
-})
-const viewImages = () =>{
-  console.log({selectedBreed, selectedSubBreed, noImages});
-  if(selectedBreed.length == 0){
-    setBreedError(true);
-  }
-  if(selectedSubBreed.length == 0 && SubBreeds.length > 0){
-    setSubBreedError(true);
-  }
-  if(noImages== 0){
-    setImagesError(true);
-  }
-  if(breedError || subBreedError || imagesError){
-    return
-  }
-  var tempImages =[];
-  setImagesToShow([]);
-  for(var i=0; i<noImages; i++){
-    tempImages.push(images[i]);
-  }
-  setImagesToShow(tempImages);
+  useEffect(() => {
+    setImages([]);
+    setSelectedNoImages(0);
+  }, [breeds, SubBreeds])
 
-}
-  
+  useEffect(() => {
+    setImagesError(false);
+  }, [noImages])
+  const imageOptions = images.map((image, index) => {
+    return {
+      value: index + 1,
+      lable: index + 1
+    }
+  })
+  const viewImages = () => {
+    if (selectedBreed.length == 0) {
+      setBreedError(true);
+    }
+    if (selectedSubBreed.length == 0 && SubBreeds.length > 0) {
+      setSubBreedError(true);
+    }
+    if (noImages == 0) {
+      setImagesError(true);
+    }
+    if (breedError || subBreedError || imagesError) {
+      return
+    }
+    var tempImages = [];
+    setImagesToShow([]);
+    for (var i = 0; i < noImages; i++) {
+      tempImages.push(images[i]);
+    }
+    setImagesToShow(tempImages);
+
+  }
+
   return (
-    <Layout className="layout" style={{height: "49rem"}}>
+    <Layout className="layout" style={{ height: "49rem" }}>
       <Header>
         <div className="logo" />
       </Header>
       <Content
         style={{
-          padding: '50px 50px', 
+          padding: '50px 50px',
         }}
       >
         <div
@@ -122,59 +113,59 @@ const viewImages = () =>{
             className="ant-advanced-search-form"
             layout="inline"
           >
-          <Form.Item label="Breed">
-            <Select
-              defaultValue={{
-                value: 'Select',
-                label: 'Select',
+            <Form.Item label="Breed">
+              <Select
+                defaultValue={{
+                  value: 'Select',
+                  label: 'Select',
                 }}
-              style={{
-                width: 120,
-              }}
-              status = {breedError ? "error" : ""}
-              onChange={(v) => setSelectedBreed(v)}
-              options={breeds}
-            />
+                style={{
+                  width: 120,
+                }}
+                status={breedError ? "error" : ""}
+                onChange={(v) => {setImagesToShow([]);setSelectedBreed(v)}}
+                options={breeds}
+              />
             </Form.Item>
-            {(!!selectedBreed && !!SubBreeds.length) && 
-            <Form.Item label="Sub Breed">
-            <Select 
-            defaultValue={{
-            value: 'Select',
-            label: 'Select',
-            }}
-            style={{
-              width: 120,
-            }}
-            status = {subBreedError ? "error" : ""}
-            onChange={(v) => setSelectedSubBreed(v)}
-            options={SubBreeds}
-            />
-            </Form.Item>
+            {(!!selectedBreed && !!SubBreeds.length) &&
+              <Form.Item label="Sub Breed">
+                <Select
+                  defaultValue={{
+                    value: 'Select',
+                    label: 'Select',
+                  }}
+                  style={{
+                    width: 120,
+                  }}
+                  status={subBreedError ? "error" : ""}
+                  onChange={(v) => {setImagesToShow([]);setSelectedSubBreed(v)}}
+                  options={SubBreeds}
+                />
+              </Form.Item>
             }
-            
-            {!!images.length && 
-            <Form.Item label="No of Images">
-            <Select
-              defaultValue={{
-                value: 'Select',
-                label: 'Select',
-                }}
-              style={{
-                width: 120,
-              }}
-              status = {imagesError ? "error" : ""}
-              onChange={(v) => setSelectedNoImages(v)}
-              options= {imageOptions}
-            />
-            </Form.Item>
+
+            {!!images.length &&
+              <Form.Item label="No of Images">
+                <Select
+                  defaultValue={{
+                    value: 'Select',
+                    label: 'Select',
+                  }}
+                  style={{
+                    width: 120,
+                  }}
+                  status={imagesError ? "error" : ""}
+                  onChange={(v) => {setImagesToShow([]);setSelectedNoImages(v)}}
+                  options={imageOptions}
+                />
+              </Form.Item>
             }
             <Button onClick={viewImages} type="primary">
               View Images
             </Button>
 
           </Form>
-          {!!noImages && imagesToShow.map(image => <img src={image} key={image} width="150" height="150" style={{margin: "2rem"}}/>)}
+          {!!noImages && imagesToShow.map(image => <img src={image} key={image} width="150" height="150" style={{ margin: "2rem" }} />)}
 
         </div>
       </Content>
